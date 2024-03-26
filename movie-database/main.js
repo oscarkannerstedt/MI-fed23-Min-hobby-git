db9486077c7d9bf07c285a143204f7f098d52ab5
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-03-26 08:43:50 +0100

    Added function to show all movies on first page

--- movie-database/main.js
+++ movie-database/main.js
@@ -1,9 +1,11 @@
 import './style.css'
 import { getAllMovies, getMoviesbyGenre } from './src/js/movies.js';
+import { showAllMovies } from './src/js/showAllMovies.js';
 
 window.addEventListener('DOMContentLoaded', async () => {
-  const movies = await getAllMovies();
-  console.log(movies);
-  const movieContainer = document.getElementById('movie-container');
+  // const movies = await getAllMovies();
+  // console.log(movies);
+  // const movieContainer = document.getElementById('movie-container');
+  showAllMovies();
   
 });
---
+++ movie-database/src/js/showAllMovies.js
@@ -0,0 +1,32 @@
+import { getAllMovies } from "./movies.js";
+
+async function showAllMovies() {
+    const movies = await getAllMovies();
+    const movieContainer = document.getElementById("movie-container");
+
+    movies.forEach(movie => {
+        const movieCard = document.createElement("div");
+        movieCard.classList.add("movie-card");
+
+        const movieTitle = document.createElement("h2");
+        movieTitle.classList.add("movie-title");
+        movieTitle.textContent = movie.title;
+
+        const movieReleaseYear = document.createElement('p');
+        movieReleaseYear.classList.add("movie-release-year");
+        movieReleaseYear.textContent = `Release Year: ${movie.release_date.substring(0, 4)}`;
+
+        const movieRating = document.createElement("p");
+        movieRating.classList.add("movie-rating");
+        movieRating.textContent = `Rating: ${movie.vote_average}`;
+
+        const moviePoster = document.createElement("img");
+        moviePoster.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+        moviePoster.alt = `${movie.title} Poster`;
+
+        movieCard.append(movieTitle, moviePoster, movieReleaseYear, movieRating);
+        movieContainer.appendChild(movieCard);
+    });
+}
+
+export { showAllMovies };
