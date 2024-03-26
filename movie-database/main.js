3fac1c262351d1e07700e278364b7e72318d6609
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-03-26 09:46:12 +0100

    Comment function

--- movie-database/main.js
+++ movie-database/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import { getAllMovies, getMoviesbyGenre } from './src/js/movies.js';
 import { showAllMovies } from './src/js/showAllMovies.js';
 
+// Show all movies on first page
 window.addEventListener('DOMContentLoaded', async () => {
   showAllMovies();
   
