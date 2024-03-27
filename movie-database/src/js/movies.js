79564e885a175859346500d33130c93b713f177f
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-03-27 10:00:51 +0100

    Added function to fetch upcoming movies

--- movie-database/src/js/movies.js
+++ movie-database/src/js/movies.js
@@ -41,4 +41,18 @@ async function getTopRatedMovies() {
     }
 }
 
-export { getAllMovies, getMoviesByGenre, getTopRatedMovies };
+async function getUpcomingMovies() {
+    const API_KEY = '2fb155d31c13f91b00f05dc941e6e6ea';
+    const API_URL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`;
+
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.error("Error while fetching movies: ", error);
+        return [];
+    }
+}
+
+export { getAllMovies, getMoviesByGenre, getTopRatedMovies, getUpcomingMovies };
