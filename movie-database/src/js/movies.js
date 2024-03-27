98b5e192e6bf2d129726e86a2e1f61dd8aeecc88
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-03-27 08:57:42 +0100

    Added function to fetch top rated movies from Api

--- movie-database/src/js/movies.js
+++ movie-database/src/js/movies.js
@@ -27,4 +27,18 @@ async function getMoviesByGenre(genreId) {
     }
 }
 
-export { getAllMovies, getMoviesByGenre };
+async function getTopRatedMovies() {
+    const API_KEY = '2fb155d31c13f91b00f05dc941e6e6ea';
+    const API_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
+
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.error("Error while fetching movies: ", error);
+        return [];
+    }
+}
+
+export { getAllMovies, getMoviesByGenre, getTopRatedMovies };
