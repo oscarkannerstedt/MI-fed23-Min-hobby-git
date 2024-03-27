e00c2b2616b419e5c27d1ec439d50b58a4b2bbca
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-03-27 09:26:12 +0100

    Added click event when click on button top-rated-button

--- movie-database/main.js
+++ movie-database/main.js
@@ -5,12 +5,14 @@ import { showActionMovies } from './src/js/showActionMovies.js';
 import { showComedyMovies } from './src/js/showComedyMovies.js';
 import { showDramaMovies } from './src/js/showDramaMovies.js';
 import { showScienceFictionMovies } from './src/js/showScienceFictionMovies.js';
+import { showTopRatedMovies } from './src/js/showTopRatedMovies.js';
 
 window.addEventListener('DOMContentLoaded', async () => {
   const actionMoviesContainer = document.getElementById("action-movies");
   const comedyMoviesContainer = document.getElementById("comedy-movies");
   const dramaMoviesContainer = document.getElementById("drama-movies");
   const scienceFictionMoviesContainer = document.getElementById("science-fiction-movies");
+  const topRatedMoviesContainer = document.getElementById("top-rated-movies");
 
 
   showAllMovies();
@@ -33,6 +35,11 @@ window.addEventListener('DOMContentLoaded', async () => {
   document.getElementById("science-fiction-button").addEventListener("click", function() {
     scienceFictionMoviesContainer.innerHTML = "";
     showScienceFictionMovies();
+  });
+
+  document.getElementById("top-rated-button").addEventListener("click", function() {
+    topRatedMoviesContainer.innerHTML = "";
+    showTopRatedMovies();
   });
   
 });
